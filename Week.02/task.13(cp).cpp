de9c602c08021f8eac85b9c34bdbd635bdfd16d8
sunil-malik-10 <maliksunil40100@gmail.com>
2024-09-22 14:51:30 +0500

    Update task.13(cp).cpp

--- Week.02/task.13(cp).cpp
+++ Week.02/task.13(cp).cpp
@@ -1,22 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
-int num1,num2;
+int num;
 int sum=0;
 cout<<"Enter 1st number:";
-cin>>num1;
-sum+=num1;
+cin>>num;
+sum=sum+num;
 cout<<"Enter 2nd number:";
-cin>>num2;
-sum+=num2;
+cin>>num;
+sum=sum+num;
 cout<<"Enter 3rd number:";
-cin>>num1;
-sum+=num1;
+cin>>num;
+sum=sum+num;
 cout<<"Enter 4th number:";
-cin>>num2;
-sum+=num2;
+cin>>num;
+sum=sum+num;
 cout<<"Enter 5th number:";
-cin>>num1;
-sum+=num1;
+cin>>num;
+sum=sum+num;
 cout<<"The sum of 5 digits is: "<<sum;
 }
